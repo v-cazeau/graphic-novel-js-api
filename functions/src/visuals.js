1d8c3de607c1c889a4822b649e15b052612a5534
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-06-29 22:14:36 -0400

    FIXED json problem

--- functions/src/visuals.js
+++ functions/src/visuals.js
@@ -2,7 +2,7 @@ import { ObjectId } from 'mongodb';
 import { db } from './dbConnect.js';
 
 export async function getAllVisuals(req, res) {
-  const coll = db.collection('visuals');
+  const coll = db.collection('visual-story-one');
   
   const visualNovel = await coll.find({}).toArray();
   res.status(200).send(visualNovel);
